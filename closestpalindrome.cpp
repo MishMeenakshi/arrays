ca75ad6f5d98754241df4d162e890135b640ba07
MisMeenakshi <33597200+MishMeenakshi@users.noreply.github.com>
2018-09-21 19:24:33 +0530

    First commit

---
+++ closestpalindrome.cpp
@@ -0,0 +1,54 @@
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+int pal(int a )
+{ int rem,rev=0;
+ int temp; temp=a; 
+  while(a!=0)
+  {
+  rem=a%10;
+  rev=(rev*10)+rem;
+  a=a/10;
+  }
+  if(temp==rev)
+   return 1;
+   else
+   return 0;
+}
+
+int main()
+{   int t,n,p,i;
+    cout<<"enter the number of test cases :"<<endl;
+    cin>>t;
+    while(t!=0)
+    { cout<<"enter any number you wish:"<<endl;
+      cin>>n;
+      if(n*10<=90)
+      {
+        cout<<"nearest palindrome is :"<<n<<endl;    }
+        else 
+    {
+          p=pal(n);
+        if(p==1) {cout<<"nearest palindrome is :"<<n<<endl; goto l1;}
+        else
+    {
+        for(i=0;i<=n;i++)
+    {     n=n-1;
+        p=pal(n);
+            if(p==1) {cout<<"nearest palindrome is :"<<n<<endl; goto l1;}
+                else {
+                n=n+2;
+             p=pal(n);
+            if(p==1) {
+			{cout<<"nearest palindrome is :"<<n<<endl; goto l1;}
+            }
+            else
+             continue;}
+    }
+        }
+            }
+    l1:  t--;
+
+}  return 0;
+}
+
